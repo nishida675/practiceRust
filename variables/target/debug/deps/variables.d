e2820f643c994a1a90068f037bbdef93cb7e03b2
nishida675 <hoge93141@gmail.com>
2024-05-20 17:18:33 +0900

    5/20

---
+++ variables/target/debug/deps/variables.d
@@ -0,0 +1,5 @@
+C:\Users\its2023\projects\practiceRust\variables\target\debug\deps\variables.exe: src\main.rs
+
+C:\Users\its2023\projects\practiceRust\variables\target\debug\deps\variables.d: src\main.rs
+
+src\main.rs:
---
+++ variables/target/debug/variables.d
@@ -0,0 +1,1 @@
+C:\Users\its2023\projects\practiceRust\variables\target\debug\variables.exe: C:\Users\its2023\projects\practiceRust\variables\src\main.rs
