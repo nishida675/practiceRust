dc2d72957cd152d2689db3eb438a8b1f923c69d1
nishida675 <hoge93141@gmail.com>
2024-05-29 17:33:56 +0900

    sssssssssss

---
+++ rectangles/target/debug/deps/rectangles.d
@@ -0,0 +1,5 @@
+C:\Users\its2023\projects\PRACTICERUST\rectangles\target\debug\deps\rectangles.exe: src\main.rs
+
+C:\Users\its2023\projects\PRACTICERUST\rectangles\target\debug\deps\rectangles.d: src\main.rs
+
+src\main.rs:
---
+++ rectangles/target/debug/rectangles.d
@@ -0,0 +1,1 @@
+C:\Users\its2023\projects\PRACTICERUST\rectangles\target\debug\rectangles.exe: C:\Users\its2023\projects\PRACTICERUST\rectangles\src\main.rs
